44aa66e3e82d0e5ff840fb3579b29a3d55d79a02
Marcello Bozzi <bozzimarcello@gmail.com>
2024-04-07 09:55:46 +0200

    added layout and two charts

---
+++ BookingDashboardWPF/BookingDashboardWPF/Prenotazioni.cs
@@ -0,0 +1,47 @@
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace BookingDashboardWPF
+{
+    public class Prenotazioni
+    {
+        public ISeries[] Series { get; set; }
+
+        public ISeries[] AnotherSeries { get; set; }
+
+        public Prenotazioni()
+        {
+            List<double> values = new List<double> { 0, 1, 2 };
+            Series = new ISeries[1];
+            Series[0] = new LineSeries<double>
+            {
+                Values = values
+            };
+
+            AnotherSeries = new ISeries[]
+            {
+                    new PieSeries<double> { Values = new double[] { 2 } },
+                    new PieSeries<double> { Values = new double[] { 4 } },
+                    new PieSeries<double> { Values = new double[] { 1 } },
+                    new PieSeries<double> { Values = new double[] { 3 } }
+            };
+        }
+
+        public void Aggiorna()
+        {
+            var r = new Random();
+            var values = Series[0].Values as List<double>;
+            values.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                values.Add(r.Next(0, 10));
+            }
+        }
+    }
+}
